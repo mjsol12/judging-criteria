eb5a5674cecdf53ea3e9c00adca75fc0fdcfa4e7
Mark Jones Solano <Mark.Solano@caseddimensions.com>
2022-06-13 01:38:07 +0800

    redesign summary

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,32 +7,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
     urlSummary: string;
-    judge1FR: string;
-    judge2FR: string;
-    judge3FR: string;
-    judge4FR: string;
-    judge5FR: string;
-    judge1QA: string;
-    judge2QA: string;
-    judge3QA: string;
-    judge4QA: string;
-    judge5QA: string;
+    JudgesFinalRound: any[];
+    JudgesQARound: any[];
   constructor() { }
 
   ngOnInit() {
       const parsedUrl = new URL(window.location.href);
       const baseUrl = parsedUrl.origin;
       this.urlSummary = `${baseUrl}/summary?admin=admin`;
-      this.judge1FR = `${baseUrl}/final-round?judge=judge_1`;
-      this.judge2FR = `${baseUrl}/final-round?judge=judge_2`;
-      this.judge3FR = `${baseUrl}/final-round?judge=judge_3`;
-      this.judge4FR = `${baseUrl}/final-round?judge=judge_4`;
-      this.judge5FR = `${baseUrl}/final-round?judge=judge_5`;
-      this.judge1QA = `${baseUrl}/question-and-answer?judge=judge_1`;
-      this.judge2QA = `${baseUrl}/question-and-answer?judge=judge_2`;
-      this.judge3QA = `${baseUrl}/question-and-answer?judge=judge_3`;
-      this.judge4QA = `${baseUrl}/question-and-answer?judge=judge_4`;
-      this.judge5QA = `${baseUrl}/question-and-answer?judge=judge_5`;
+      this.JudgesFinalRound = [
+          {
+              id: 'judge1FR',
+              url: `${baseUrl}/final-round?judge=judge_1`,
+              name: 'Judge 1'
+          },
+          {
+              id: 'judge2FR',
+              url: `${baseUrl}/final-round?judge=judge_2`,
+              name: 'Judge 2'
+          },
+          {
+              id: 'judge3FR',
+              url: `${baseUrl}/final-round?judge=judge_3`,
+              name: 'Judge 3'
+          },
+          {
+              id: 'judge4FR',
+              url: `${baseUrl}/final-round?judge=judge_4`,
+              name: 'Judge 4'
+          },
+          {
+              id: 'judge5FR',
+              url: `${baseUrl}/final-round?judge=judge_5`,
+              name: 'Judge 5'
+          }
+      ];
+
+      this.JudgesQARound = [
+          {
+              id: 'judge1FR',
+              url: `${baseUrl}/question-and-answer?judge=judge_1`,
+              name: 'Judge 1'
+          },
+          {
+              id: 'judge2FR',
+              url: `${baseUrl}/question-and-answer?judge=judge_2`,
+              name: 'Judge 2'
+          },
+          {
+              id: 'judge3FR',
+              url: `${baseUrl}/question-and-answer?judge=judge_3`,
+              name: 'Judge 3'
+          },
+          {
+              id: 'judge4FR',
+              url: `${baseUrl}/question-and-answer?judge=judge_4`,
+              name: 'Judge 4'
+          },
+          {
+              id: 'judge5FR',
+              url: `${baseUrl}/question-and-answer?judge=judge_5`,
+              name: 'Judge 5'
+          }
+      ];
   }
 
 }
